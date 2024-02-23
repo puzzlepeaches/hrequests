c743f60f35506fb7b28a39a5ffdff79355ebc818
puzzlepeaches <nanastasi@sprocketsecurity.com>
2024-02-23 08:45:39 -0600

    Modifications

--- hrequests/cffi.py
+++ hrequests/cffi.py
@@ -30,9 +30,7 @@ arch_map = {
 
 class LibraryManager:
     def __init__(self):
-        # self.parent_path = os.path.join(root_dir, 'bin')
-        os.makedirs('/tmp/bin', exist_ok=True)
-        self.parent_path = '/tmp/bin'
+        self.parent_path = os.path.join(root_dir, 'bin')
         self.file_cont, self.file_ext = self.get_name()
         filename = self.check_library()
         self.full_path = os.path.join(self.parent_path, filename)
